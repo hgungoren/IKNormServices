954fc134b4936eb9c8fc6cc9a9fe38a2cdb6bc3d
Emre Can Ayar <emre.ayar@suratkargo.com.tr>
2021-11-09 10:56:51 +0300

    Ldap konfigurasyon

--- src/Serendip.IK.Core/Editions/EditionManager.cs
+++ src/Serendip.IK.Core/Editions/EditionManager.cs
@@ -1,6 +1,7 @@
 ﻿using Abp.Application.Editions;
 using Abp.Application.Features;
 using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
 
 namespace Serendip.IK.Editions
 {
@@ -9,11 +10,11 @@ namespace Serendip.IK.Editions
         public const string DefaultEditionName = "Standard";
 
         public EditionManager(
-            IRepository<Edition> editionRepository, 
-            IAbpZeroFeatureValueStore featureValueStore)
+            IRepository<Edition> editionRepository,
+            IAbpZeroFeatureValueStore featureValueStore, IUnitOfWorkManager unitOfWorkManager)
             : base(
                 editionRepository,
-                featureValueStore)
+                featureValueStore, unitOfWorkManager)
         {
         }
     }
---
+++ src/Serendip.IK.Core/Ldap/SuratLdapAuthenticationSource.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Zero.Ldap.Authentication;
+using Abp.Zero.Ldap.Configuration;
+using Serendip.IK.Authorization.Users;
+using Serendip.IK.MultiTenancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serendip.IK.Ldap
+{
+    public class SuratLdapAuthenticationSource : LdapAuthenticationSource<Tenant, User>
+    {
+        public SuratLdapAuthenticationSource(ILdapSettings settings, IAbpZeroLdapModuleConfig ldapModuleConfig)
+           : base(settings, ldapModuleConfig)
+        {
+        }
+    }
+}
