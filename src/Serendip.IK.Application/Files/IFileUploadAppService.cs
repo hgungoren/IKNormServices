45695dde7708599ae7282f7acc96e53930868b21
Murat Vuranok <murat.vuranok@suratkargo.com.tr>
2021-11-29 15:31:14 +0300

    releaseEdit

--- src/Serendip.IK.Application/Files/IFileUploadAppService.cs
+++ src/Serendip.IK.Application/Files/IFileUploadAppService.cs
@@ -2,16 +2,14 @@
 using Serendip.IK.File.Dto;
 using Serendip.IK.Files.Dto;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Serendip.IK.File
 {
     public interface IFileUploadAppService:IApplicationService
     {
-        //Task<FileUploadResultDto> Upload(FileUploadDto dto);
-        //Task<FileDownloadDto> Download(Guid id);
-        //Task DeleteFile (string path);
+        Task<FileUploadResultDto> Upload(FileUploadDto dto);
+        Task<FileDownloadDto> Download(long id);
+        Task DeleteFile(string path);
     }
 }
